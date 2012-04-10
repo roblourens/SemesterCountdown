3dd3f3681e88755f0a4cd115efd0c551d91369c1
rob <roblourens@gmail.com>
2012-04-10 00:57:55 -0500

    Actually, that extra stuff on the gradient css was necessary. And set min-height for the social container so it doesn't overlap the timeline

--- Conf.js
+++ Conf.js
@@ -12,24 +12,24 @@ Conf.init = function(confJson)
     this.timelineFuture = confJson['timelineFuture'];
     this.name = confJson['name'];
 
-    /* Old browsers */
+    // Old browsers
     $('body').css('background', this.primaryColor);
-    /* FF3.6+ */
-    $('body').css('background', '-moz-linear-gradient(top, '+this.primaryColor+' 0%, '+this.gradBottom+' 100%)');
-    /* Chrome,Safari4+ */
-    $('body').css('background', '-webkit-gradient(linear, left top, left bottom, color-stop(0%,'+this.primaryColor+'), color-stop(100%,'+this.gradBottom+'))'); 
-    /* Chrome10+,Safari5.1+ */
-    $('body').css('background', '-webkit-linear-gradient(top, '+this.primaryColor+' 0%,'+this.gradBottom+' 100%)');
-    /* Opera 11.10+ */
-    $('body').css('background', '-o-linear-gradient(top, '+this.primaryColor+' 0%,'+this.gradBottom+' 100%)');
-    /* IE10+ */
-    $('body').css('background', '-ms-linear-gradient(top, '+this.primaryColor+' 0%,'+this.gradBottom+' 100%)');
-    /* IE10+ */
-    $('body').css('background', '-ms-linear-gradient(top, rgba('+this.primaryColor+') 0%,rgba('+this.gradBottom+') 100%)');
-    /* W3C */
-    $('body').css('background', 'linear-gradient(top, '+this.primaryColor+' 0%,'+this.gradBottom+' 100%)');
-    /* IE6-9 */
-    $('body').css('filter', 'progid:DXImageTransform.Microsoft.gradient( startColorstr='+this.primaryColor+', endColorstr='+this.gradBottom+',GradientType=0 )'); 
+    // FF3.6+
+    $('body').css('background', '-moz-linear-gradient(top, '+this.primaryColor+' 0%, '+this.gradBottom+' 100%) no-repeat center center fixed');
+    // Chrome,Safari4+
+    $('body').css('background', '-webkit-gradient(linear, left top, left bottom, color-stop(0%,'+this.primaryColor+'), color-stop(100%,'+this.gradBottom+')) no-repeat center center fixed'); 
+    // Chrome10+,Safari5.1+
+    $('body').css('background', '-webkit-linear-gradient(top, '+this.primaryColor+' 0%,'+this.gradBottom+' 100%) no-repeat center center fixed');
+    // Opera 11.10+
+    $('body').css('background', '-o-linear-gradient(top, '+this.primaryColor+' 0%,'+this.gradBottom+' 100%) no-repeat center center fixed');
+    // IE10+
+    $('body').css('background', '-ms-linear-gradient(top, '+this.primaryColor+' 0%,'+this.gradBottom+' 100%) no-repeat center center fixed');
+    // IE10+
+    $('body').css('background', '-ms-linear-gradient(top, rgba('+this.primaryColor+') 0%,rgba('+this.gradBottom+') 100%) no-repeat center center fixed');
+    // W3C
+    $('body').css('background', 'linear-gradient(top, '+this.primaryColor+' 0%,'+this.gradBottom+' 100%) no-repeat center center fixed');
+    // IE6-9
+    $('body').css('filter', 'progid:DXImageTransform.Microsoft.gradient( startColorstr='+this.primaryColor+', endColorstr='+this.gradBottom+',GradientType=0 ) no-repeat center center fixed'); 
 
     $('body').css('color', this.secondaryColor);
     $('body').css('text-shadow', '1px 1px 0px '+this.shadowColor);
