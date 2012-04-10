e94ddef35fde77e95a4438d4a3ec7e982d701789
rob <roblourens@gmail.com>
2012-04-09 23:15:08 -0500

    accidentally deleted GA

--- index.php
+++ index.php
@@ -9,6 +9,17 @@
 <meta property="fb:admins" content="500029869" />
 <script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
 <script type="text/javascript">stLight.options({publisher: "92b1d824-e709-4b9a-a588-3fec47e291d8"}); </script>
+<script type="text/javascript">
+ var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-28941284-1']);
+  _gaq.push(['_setDomainName', '.semestercountdown.com']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
 </head>
 <body>
 <script type="text/javascript" src="moment.min.js"></script>
